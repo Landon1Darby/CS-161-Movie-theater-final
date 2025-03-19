4874c9ed0321e61a7109d8aa388f4dcd6f367c88
Landon Darby <kelsodarby732@gmail.com>
2025-03-18 23:44:25 -0700

    Add names to listbox  make price random  Populate Dictionary with indexes int index = indexPairs["name"]; fixxed error check if genre matches and remove the ones that dont

--- CS 161 Movie theater final/Form1.Designer.cs
+++ CS 161 Movie theater final/Form1.Designer.cs
@@ -30,28 +30,28 @@
         {
             movieListbox = new ListBox();
             groupBox1 = new GroupBox();
-            SearchGroupbox = new GroupBox();
-            textBox1 = new TextBox();
-            searchButton = new Button();
-            priceBox = new TextBox();
-            label1 = new Label();
-            famliyCheck = new CheckBox();
-            romanceCheck = new CheckBox();
-            scaryCheck = new CheckBox();
-            comedyCheck = new CheckBox();
-            label2 = new Label();
-            label3 = new Label();
-            nameOutput = new Label();
-            moviePoster = new PictureBox();
-            label5 = new Label();
-            label6 = new Label();
+            descriptionOutput = new Label();
+            label10 = new Label();
             costOutput = new Label();
             label8 = new Label();
-            label10 = new Label();
-            descriptionOutput = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            moviePoster = new PictureBox();
+            nameOutput = new Label();
+            label3 = new Label();
+            SearchGroupbox = new GroupBox();
+            label2 = new Label();
+            comedyCheck = new CheckBox();
+            scaryCheck = new CheckBox();
+            romanceCheck = new CheckBox();
+            famliyCheck = new CheckBox();
+            label1 = new Label();
+            priceBox = new TextBox();
+            searchButton = new Button();
+            textBox1 = new TextBox();
             groupBox1.SuspendLayout();
-            SearchGroupbox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)moviePoster).BeginInit();
+            SearchGroupbox.SuspendLayout();
             SuspendLayout();
             // 
             // movieListbox
@@ -61,6 +61,7 @@
             movieListbox.Name = "movieListbox";
             movieListbox.Size = new Size(247, 164);
             movieListbox.TabIndex = 0;
+            movieListbox.SelectedIndexChanged += movieListbox_SelectedIndexChanged;
             // 
             // groupBox1
             // 
@@ -80,148 +81,23 @@
             groupBox1.TabStop = false;
             groupBox1.Text = "Details";
             // 
-            // SearchGroupbox
+            // descriptionOutput
             // 
-            SearchGroupbox.Controls.Add(label2);
-            SearchGroupbox.Controls.Add(comedyCheck);
-            SearchGroupbox.Controls.Add(scaryCheck);
-            SearchGroupbox.Controls.Add(romanceCheck);
-            SearchGroupbox.Controls.Add(famliyCheck);
-            SearchGroupbox.Controls.Add(label1);
-            SearchGroupbox.Controls.Add(priceBox);
-            SearchGroupbox.Controls.Add(searchButton);
-            SearchGroupbox.Controls.Add(textBox1);
-            SearchGroupbox.Location = new Point(12, 200);
-            SearchGroupbox.Name = "SearchGroupbox";
-            SearchGroupbox.Size = new Size(247, 209);
-            SearchGroupbox.TabIndex = 2;
-            SearchGroupbox.TabStop = false;
-            SearchGroupbox.Text = "Search";
+            descriptionOutput.BackColor = SystemColors.Control;
+            descriptionOutput.BorderStyle = BorderStyle.FixedSingle;
+            descriptionOutput.Location = new Point(15, 201);
+            descriptionOutput.Name = "descriptionOutput";
+            descriptionOutput.Size = new Size(258, 182);
+            descriptionOutput.TabIndex = 18;
             // 
-            // textBox1
+            // label10
             // 
-            textBox1.Location = new Point(69, 26);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(158, 27);
-            textBox1.TabIndex = 0;
-            // 
-            // searchButton
-            // 
-            searchButton.Location = new Point(88, 174);
-            searchButton.Name = "searchButton";
-            searchButton.Size = new Size(70, 29);
-            searchButton.TabIndex = 1;
-            searchButton.Text = "Search";
-            searchButton.UseVisualStyleBackColor = true;
-            searchButton.Click += searchButton_Click;
-            // 
-            // priceBox
-            // 
-            priceBox.Location = new Point(69, 59);
-            priceBox.Name = "priceBox";
-            priceBox.Size = new Size(158, 27);
-            priceBox.TabIndex = 2;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(11, 66);
-            label1.Name = "label1";
-            label1.Size = new Size(44, 20);
-            label1.TabIndex = 4;
-            label1.Text = "Price:";
-            // 
-            // famliyCheck
-            // 
-            famliyCheck.AutoSize = true;
-            famliyCheck.Location = new Point(11, 103);
-            famliyCheck.Name = "famliyCheck";
-            famliyCheck.Size = new Size(73, 24);
-            famliyCheck.TabIndex = 5;
-            famliyCheck.Text = "Famliy";
-            famliyCheck.UseVisualStyleBackColor = true;
-            // 
-            // romanceCheck
-            // 
-            romanceCheck.AutoSize = true;
-            romanceCheck.Location = new Point(118, 103);
-            romanceCheck.Name = "romanceCheck";
-            romanceCheck.Size = new Size(93, 24);
-            romanceCheck.TabIndex = 6;
-            romanceCheck.Text = "Romance";
-            romanceCheck.UseVisualStyleBackColor = true;
-            // 
-            // scaryCheck
-            // 
-            scaryCheck.AutoSize = true;
-            scaryCheck.Location = new Point(11, 133);
-            scaryCheck.Name = "scaryCheck";
-            scaryCheck.Size = new Size(75, 24);
-            scaryCheck.TabIndex = 7;
-            scaryCheck.Text = "Horror";
-            scaryCheck.UseVisualStyleBackColor = true;
-            // 
-            // comedyCheck
-            // 
-            comedyCheck.AutoSize = true;
-            comedyCheck.Location = new Point(118, 133);
-            comedyCheck.Name = "comedyCheck";
-            comedyCheck.Size = new Size(86, 24);
-            comedyCheck.TabIndex = 8;
-            comedyCheck.Text = "Comedy";
-            comedyCheck.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(11, 29);
-            label2.Name = "label2";
-            label2.Size = new Size(52, 20);
-            label2.TabIndex = 9;
-            label2.Text = "Name:";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(149, 26);
-            label3.Name = "label3";
-            label3.Size = new Size(52, 20);
-            label3.TabIndex = 10;
-            label3.Text = "Name:";
-            // 
-            // nameOutput
-            // 
-            nameOutput.BackColor = SystemColors.Control;
-            nameOutput.BorderStyle = BorderStyle.FixedSingle;
-            nameOutput.Location = new Point(151, 46);
-            nameOutput.Name = "nameOutput";
-            nameOutput.Size = new Size(131, 25);
-            nameOutput.TabIndex = 11;
-            // 
-            // moviePoster
-            // 
-            moviePoster.Location = new Point(15, 26);
-            moviePoster.Name = "moviePoster";
-            moviePoster.Size = new Size(128, 133);
-            moviePoster.TabIndex = 12;
-            moviePoster.TabStop = false;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(194, 139);
-            label5.Name = "label5";
-            label5.Size = new Size(0, 20);
-            label5.TabIndex = 13;
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(15, 171);
-            label6.Name = "label6";
-            label6.Size = new Size(88, 20);
-            label6.TabIndex = 14;
-            label6.Text = "Description:";
+            label10.AutoSize = true;
+            label10.Location = new Point(151, 118);
+            label10.Name = "label10";
+            label10.Size = new Size(55, 20);
+            label10.TabIndex = 17;
+            label10.Text = "Rating:";
             // 
             // costOutput
             // 
@@ -241,23 +117,148 @@
             label8.TabIndex = 15;
             label8.Text = "Cost:";
             // 
-            // label10
+            // label6
             // 
-            label10.AutoSize = true;
-            label10.Location = new Point(151, 118);
-            label10.Name = "label10";
-            label10.Size = new Size(55, 20);
-            label10.TabIndex = 17;
-            label10.Text = "Rating:";
+            label6.AutoSize = true;
+            label6.Location = new Point(15, 171);
+            label6.Name = "label6";
+            label6.Size = new Size(88, 20);
+            label6.TabIndex = 14;
+            label6.Text = "Description:";
             // 
-            // descriptionOutput
+            // label5
             // 
-            descriptionOutput.BackColor = SystemColors.Control;
-            descriptionOutput.BorderStyle = BorderStyle.FixedSingle;
-            descriptionOutput.Location = new Point(15, 201);
-            descriptionOutput.Name = "descriptionOutput";
-            descriptionOutput.Size = new Size(258, 182);
-            descriptionOutput.TabIndex = 18;
+            label5.AutoSize = true;
+            label5.Location = new Point(194, 139);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 20);
+            label5.TabIndex = 13;
+            // 
+            // moviePoster
+            // 
+            moviePoster.Location = new Point(15, 26);
+            moviePoster.Name = "moviePoster";
+            moviePoster.Size = new Size(128, 133);
+            moviePoster.TabIndex = 12;
+            moviePoster.TabStop = false;
+            // 
+            // nameOutput
+            // 
+            nameOutput.BackColor = SystemColors.Control;
+            nameOutput.BorderStyle = BorderStyle.FixedSingle;
+            nameOutput.Location = new Point(151, 46);
+            nameOutput.Name = "nameOutput";
+            nameOutput.Size = new Size(131, 25);
+            nameOutput.TabIndex = 11;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(149, 26);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 20);
+            label3.TabIndex = 10;
+            label3.Text = "Name:";
+            // 
+            // SearchGroupbox
+            // 
+            SearchGroupbox.Controls.Add(label2);
+            SearchGroupbox.Controls.Add(comedyCheck);
+            SearchGroupbox.Controls.Add(scaryCheck);
+            SearchGroupbox.Controls.Add(romanceCheck);
+            SearchGroupbox.Controls.Add(famliyCheck);
+            SearchGroupbox.Controls.Add(label1);
+            SearchGroupbox.Controls.Add(priceBox);
+            SearchGroupbox.Controls.Add(searchButton);
+            SearchGroupbox.Controls.Add(textBox1);
+            SearchGroupbox.Location = new Point(12, 200);
+            SearchGroupbox.Name = "SearchGroupbox";
+            SearchGroupbox.Size = new Size(247, 209);
+            SearchGroupbox.TabIndex = 2;
+            SearchGroupbox.TabStop = false;
+            SearchGroupbox.Text = "Search";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 29);
+            label2.Name = "label2";
+            label2.Size = new Size(52, 20);
+            label2.TabIndex = 9;
+            label2.Text = "Name:";
+            // 
+            // comedyCheck
+            // 
+            comedyCheck.AutoSize = true;
+            comedyCheck.Location = new Point(118, 133);
+            comedyCheck.Name = "comedyCheck";
+            comedyCheck.Size = new Size(86, 24);
+            comedyCheck.TabIndex = 8;
+            comedyCheck.Text = "Comedy";
+            comedyCheck.UseVisualStyleBackColor = true;
+            // 
+            // scaryCheck
+            // 
+            scaryCheck.AutoSize = true;
+            scaryCheck.Location = new Point(11, 133);
+            scaryCheck.Name = "scaryCheck";
+            scaryCheck.Size = new Size(75, 24);
+            scaryCheck.TabIndex = 7;
+            scaryCheck.Text = "Horror";
+            scaryCheck.UseVisualStyleBackColor = true;
+            // 
+            // romanceCheck
+            // 
+            romanceCheck.AutoSize = true;
+            romanceCheck.Location = new Point(118, 103);
+            romanceCheck.Name = "romanceCheck";
+            romanceCheck.Size = new Size(93, 24);
+            romanceCheck.TabIndex = 6;
+            romanceCheck.Text = "Romance";
+            romanceCheck.UseVisualStyleBackColor = true;
+            // 
+            // famliyCheck
+            // 
+            famliyCheck.AutoSize = true;
+            famliyCheck.Location = new Point(11, 103);
+            famliyCheck.Name = "famliyCheck";
+            famliyCheck.Size = new Size(73, 24);
+            famliyCheck.TabIndex = 5;
+            famliyCheck.Text = "Famliy";
+            famliyCheck.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(11, 66);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Price:";
+            // 
+            // priceBox
+            // 
+            priceBox.Location = new Point(69, 59);
+            priceBox.Name = "priceBox";
+            priceBox.Size = new Size(158, 27);
+            priceBox.TabIndex = 2;
+            // 
+            // searchButton
+            // 
+            searchButton.Location = new Point(88, 174);
+            searchButton.Name = "searchButton";
+            searchButton.Size = new Size(70, 29);
+            searchButton.TabIndex = 1;
+            searchButton.Text = "Search";
+            searchButton.UseVisualStyleBackColor = true;
+            searchButton.Click += searchButton_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(69, 26);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(158, 27);
+            textBox1.TabIndex = 0;
             // 
             // Form1
             // 
@@ -272,9 +273,9 @@
             Load += Form1_Load;
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)moviePoster).EndInit();
             SearchGroupbox.ResumeLayout(false);
             SearchGroupbox.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)moviePoster).EndInit();
             ResumeLayout(false);
         }
 
--- CS 161 Movie theater final/Form1.cs
+++ CS 161 Movie theater final/Form1.cs
@@ -5,7 +5,10 @@ namespace CS_161_Movie_theater_final
         List<string> movieNames;
         List<string> genres;
         List<string> descriptions;
+        List<decimal> prices = new List<decimal>();
         List<int> ratings = new List<int>();
+
+        Dictionary<string, int> indexPairs = new Dictionary<string, int>();
 
 
 
@@ -17,22 +20,43 @@ namespace CS_161_Movie_theater_final
         private void Form1_Load(object sender, EventArgs e)
         {
             // Initialize Lists (Read from files)
-            movieNames = ReadFile("movie_names");
-            genres = ReadFile("movie_genres");
-            descriptions = ReadFile("movie_descriptions");
+            movieNames = ReadFile("movies_names");
+            genres = ReadFile("movies_genres");
+            descriptions = ReadFile("movies_descriptions");
             // Convert string list to int
-            List<string> temp = ReadFile("movie_ratings");
+            List<string> temp = ReadFile("movies_ratings");
             foreach (string r in temp)
             {
                 int num = 0;
-                if (int.TryParse(r, out num))
+                if (!int.TryParse(r, out num))
                     MessageBox.Show("Parsing Error");
                 ratings.Add(num);
-            } 
+            }
+            // make price random
+            Random rand = new Random();
+            for (int i = 0; i < genres.Count; i++)
+            {
+                prices.Add(rand.Next(50, 100) / 10.0m);
+            }
 
-            
+            // Populate Dictionary with indexes
+            for (int i = 0; i < movieNames.Count(); i++)
+            {
+                indexPairs.Add(movieNames[i], i);
+            }
 
-            
+            RenderListBox(movieNames);
+        }
+
+        private void RenderListBox(List<string> list)
+        {
+            movieListbox.Items.Clear();
+            // Add names to listbox
+            foreach (string s in list)
+            {
+                movieListbox.Items.Add(s);
+            }
+
         }
 
         private List<string> ReadFile(string filename)
@@ -52,6 +76,72 @@ namespace CS_161_Movie_theater_final
 
         private void searchButton_Click(object sender, EventArgs e)
         {
+            List<string> tempNames = movieNames;
+            List<string> tempGenres = genres;
+            List<decimal> tempPrices = prices;
+
+              
+
+
+            MessageBox.Show(tempNames.Count().ToString());
+
+            // 1) Check if name search matches
+            for (int i = 0; i < tempNames.Count(); i++)
+            {
+
+            }
+
+
+            // 2) Check if price range is good
+            for (int i = 0; i < tempNames.Count(); i++)
+            {
+                decimal userInput = decimal.Parse(priceBox.Text);
+
+                if (tempPrices[i] > userInput)
+                {
+                    tempNames.RemoveAt(i);
+                    tempGenres.RemoveAt(i);
+                    tempPrices.RemoveAt(i);
+                    i--;
+                }
+
+            }
+
+
+            // 3) Check if Genre Matches
+
+            //loop through all movies
+            // create string variable that holds which genre current movie is
+            // if genre doesnt match the genre selected, remove movie
+            for (int i = 0; i < tempNames.Count; i++)
+            {
+                string[] genreList = {"","","",""};
+                if (scaryCheck.Checked) genreList[0] = "Horror";
+                if (romanceCheck.Checked) genreList[1] = "Romance";
+                if (comedyCheck.Checked) genreList[2] = "Comedy";
+                if (famliyCheck.Checked) genreList[3] = "Family";
+
+                if (genreList[0] == genres[i] || 
+                    genreList[1] == genres[i] ||
+                    genreList[2] == genres[i] ||
+                    genreList[3] == genres[i]) 
+                {
+                    
+                }
+            }
+
+            MessageBox.Show(tempNames.Count().ToString());
+            RenderListBox(tempNames);
+        }
+
+        private void movieListbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // int index = indexPairs["name"];
+            int index = indexPairs[movieListbox.Items[movieListbox.SelectedIndex].ToString()];
+            
+            nameOutput.Text = movieNames[index];
+            costOutput.Text = prices[index].ToString();
+            descriptionOutput.Text = descriptions[index];
             
         }
     }
